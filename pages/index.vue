cb8474294889c087a5cc425a5b839611a4a38613
John MBP <john@users.noreplay.github.com>
2023-07-16 00:23:58 +0400

    prettier eslint

---
+++ pages/index.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>{{ testVar }}</div>
+</template>
+<script setup>
+const testVar = ref('old value')
+onMounted(() => {
+  testVar.value = 'new value'
+  console.log('onMounted')
+})
+</script>
